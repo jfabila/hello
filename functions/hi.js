f667ec06fc4c1f973017985d8755c9a02c243b0c
Jonathan Romero <jonathan@elmatadito.com>
2019-05-07 17:23:16 -0500

    Update hi.js

--- functions/hi.js
+++ functions/hi.js
@@ -1,5 +1,8 @@
 exports.handler = function(event, context, callback) {
-    console.log('Function called by http ' + context.httpMethod);
+    console.log(event);
+    console.log(context);
+    console.log(callback);
+    console.log('Function called by http ' + event.http_method);
     if (context.httpMethod === 'POST') {
         console.log('We receive');
         callback(null, {
