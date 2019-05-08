e3ab696f3b006f8b97c2b6282d5543a84e2aac1d
Jonathan Romero <jonathan@elmatadito.com>
2019-05-08 08:26:25 -0500

    Update hi.js

--- functions/hi.js
+++ functions/hi.js
@@ -1,9 +1,10 @@
 exports.handler = function(event, context, callback) {
-    // console.log(event);
+    console.log(event);
     // console.log(context);
     // console.log(callback);
     console.log('Function called by http ' + event.httpMethod);
     if (event.httpMethod === 'POST') {
+        console.log(event.params('email'))
         callback(null, { statusCode: 200, body: 'Ok, you can call me!' });
     }
     callback(null, { statusCode: 401, body: 'Sorry, you can´t call me' });
