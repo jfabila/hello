8606b456eb2f241d7ad385ed7aec380bc1cb5b8f
Jonathan Romero <jonathan@elmatadito.com>
2019-05-08 08:36:33 -0500

    Get query string parameters from infusionsoft

--- functions/hi.js
+++ functions/hi.js
@@ -1,11 +1,20 @@
 exports.handler = function(event, context, callback) {
-    console.log(event);
+    // console.log(event);
     // console.log(context);
     // console.log(callback);
-    console.log('Function called by http ' + event.httpMethod);
+    //console.log('Function called by http ' + event.httpMethod);
     if (event.httpMethod === 'POST') {
-        console.log(event.queryStringParameters['email'])
-        callback(null, { statusCode: 200, body: 'Ok, you can call me!' });
+        
+        const cid = event.queryStringParameters['CID'] || 0;
+        const email = event.queryStringParameters['email'] || '';
+        const firstName = event.queryStringParameters['first_name'] || '';
+        const lastName = event.queryStringParameters['last_name'] || '';
+
+        const infusionSoftObject = {
+            cid, email, firstName, lastName
+        }
+
+        callback(null, { statusCode: 200, body: infusionSoftObject });
     }
     callback(null, { statusCode: 401, body: 'Sorry, you can´t call me' });
 };
